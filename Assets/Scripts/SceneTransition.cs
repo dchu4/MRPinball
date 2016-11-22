5199c231fbba8a2bafa589639a534adb74293cd6
Daniel Chu <danchu123@gmail.com>
2016-11-22 14:26:02 -0600

    Finished Version 2

---
+++ Assets/Scripts/SceneTransition.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class SceneTransition : MonoBehaviour {
+	public string sceneLoad = "";
+	GameObject gameController;
+	// Update is called once per frame
+	void Start(){
+		gameController = GameObject.FindGameObjectWithTag ("GameController");
+	}
+
+	public void SceneChange () {
+		SceneManager.LoadScene (sceneLoad);
+		Destroy (gameController.gameObject);
+	}
+}
